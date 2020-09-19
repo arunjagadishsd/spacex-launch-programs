ed4a55069a03c5efa3d7e776d55e1158cf911075
AJ <arunjagadishsd@gmail.com>
2020-09-19 18:06:51 +0530

    Add home container to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import Home from "./containers/home.container";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
