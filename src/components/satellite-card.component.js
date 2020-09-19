939cf53bfcf31895d763b7096361ac7c45b952b5
AJ <arunjagadishsd@gmail.com>
2020-09-19 18:05:33 +0530

    Add satellite card component

---
+++ src/components/satellite-card.component.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const SatelliteCard = ({ satellite }) => {
+  return (
+    <div className="card">
+      <img src={satellite.links.mission_patch_small} alt="" />
+      <h3>
+        {satellite.mission_name} #{satellite.flight_number}
+      </h3>
+      <p>
+        <strong> Mission Ids: </strong>
+      </p>
+      <ul>
+        {satellite.mission_id.map((id) => (
+          <li key={id}>{id}</li>
+        ))}
+      </ul>
+      <p>
+        <strong> Launch Year: </strong> {satellite.launch_year}
+      </p>
+      <p>
+        <strong> Successful Launch: </strong> {satellite.launch_success + ""}
+      </p>
+    </div>
+  );
+};
+
+export default SatelliteCard;
